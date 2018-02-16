5750f079e4f66068cdb8767d59462053caa22abf
Sema <bushido-95@mail.ru>
2018-02-17 01:56:10 +0300

    Вёрстка layout, главной страницы, страницы архива новостей. Создана модель News.

---
+++ FourSeasons/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FourSeasons.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ FourSeasons/Models/News.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FourSeasons.Models
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Header { get; set; }
+        public string Content { get; set; }
+        public string ImgLink { get; set; }
+    }
+}
